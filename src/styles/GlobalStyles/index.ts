f622d3ffd13ccea8a72f97d5d6358427d911cb9d
Joseph Matheus <josephmatheus@hotmail.com>
2024-03-31 19:47:39 -0300

    :lipstick: feat: Estilos globais

---
+++ src/styles/GlobalStyles/index.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+import "../../styles/variables.css"
+
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: var(--font-family);
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    height: 100vh;
+    width: 100%;
+    background-color: var(--dark-10);
+    color: var(--dark-50);
+    overflow-x: hidden;
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  li {
+    list-style: none;
+  }
+`;
