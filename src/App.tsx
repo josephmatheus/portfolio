5a1f488077ddeb289f7269c1bad503648bd523c4
Joseph Matheus <josephmatheus@hotmail.com>
2024-03-31 19:48:04 -0300

    Visualização do App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
+};
+
+export default App;
