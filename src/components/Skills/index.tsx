cf1d1c9bc7185be5f253496e92988e5ef5b76335
Joseph Matheus <josephmatheus@hotmail.com>
2024-05-31 09:40:44 -0300

    :sparkles: feat: Adiciona componente Skills

---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,22 @@
+import { IconContext } from "react-icons";
+import * as S from "./styled"
+import * as Si from "react-icons/si";
+
+export const SkillsSection = () => {
+  // const iconSize = 
+  return (
+    <S.Container>
+      <h2>Minhas Skills</h2>
+      <S.Skills>
+        <IconContext.Provider value={{className: "react-icons"}}>
+          <li><Si.SiHtml5 /></li>
+          <li><Si.SiCss3 /></li>
+          <li><Si.SiJavascript /></li>
+          <li><Si.SiReact /></li>
+          <li><Si.SiTypescript /></li>
+          <li><Si.SiStyledcomponents /></li>
+        </IconContext.Provider>
+      </S.Skills>
+    </S.Container>
+  )
+}
