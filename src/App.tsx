935562fdecfef53018091192154ada1f9c1dc17a
Joseph Matheus <josephmatheus@hotmail.com>
2024-05-31 09:43:31 -0300

    :sparkles: feat: Adiciona novo componente ao App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { AboutSection } from "./components/About";
 import { Header } from "./components/Header";
 import { HeroSection } from "./components/HeroBanner";
 import ProjectsSection from "./components/Projects";
+import { SkillsSection } from "./components/Skills";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
+      <SkillsSection />
     </>
   );
 };
