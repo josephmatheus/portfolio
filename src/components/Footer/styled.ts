1f81294d4fff688b4fe6473db5f09a73cb21b311
Joseph Matheus <josephmatheus@hotmail.com>
2024-06-03 09:06:57 -0300

    :lipstick: feat: Estilização do componente Footer

---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import "../../styles/variables.css";
+
+export const Footer = styled.footer`
+  background-color: var(--dark-20);
+  font-size: 1.6rem;
+  padding: 25px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
