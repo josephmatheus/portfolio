f25e09a9c5e156c7a1700cddcf735bee2fa475b7
Joseph Matheus <josephmatheus@hotmail.com>
2024-06-03 09:06:38 -0300

    :sparkles: feat: Adiciona componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styled"
+
+const Footer = () => {
+  return (
+    <S.Footer>
+        <p>&copy;2024</p>
+        <p>Joseph Matheus</p>
+    </S.Footer>
+  )
+}
+
+export default Footer
