95b32d66a7eab4a27d14285dc4a219c5b4a7285d
Joseph Matheus <josephmatheus@hotmail.com>
2024-06-07 09:44:45 -0300

    :lipstick: fix: Ajustando card de projeto para incluir os novos botões

--- src/components/CardProject/styled.ts
+++ src/components/CardProject/styled.ts
@@ -3,7 +3,7 @@ import "../../styles/variables.css"
 
 export const Li = styled.li`
     width: 350px;
-    height: 240px;
+    height: 296px;
     padding: 20px 25px;
     background-color: var(--dark-20);
     border: 1px solid var(--dark-30);
@@ -25,15 +25,38 @@ export const ProjectInfoContainer = styled.div`
     margin-top: 23px;
     display: flex;
     flex-direction: column;
-    gap: 6px;
 
     h3 {
         font-weight: 500;
-        font-size: 1.8rem;
+        font-size: 1.6rem;
     }
 
     p {
-        font-size: 1.4rem;
-        color: var(--dark-40);
+        font-size: 1.2rem;
+        color: var(--brand-color);
+        margin: 18px 0;
+    }
+
+    .linksContainer {
+        width: 100%;
+        display: flex;
+        gap: 18px;
+
+        a {
+            width: 140px;
+            padding: 10px;
+            font-size: 1.2rem;
+            text-align: center;
+            border: 2px solid var(--brand-color);
+            color: var(--brand-color);
+            border-radius: 4px;
+            font-weight: 700;
+
+            &:first-child {
+                color: var(--dark-10);
+                background-color: var(--brand-color);
+                
+            }
+        }
     }
 `
